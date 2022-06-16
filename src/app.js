67b170ab7d63f279a3c766af0997546eebfbff44
Nikola Majksner <nikola@airshaper.com>
2022-06-16 10:31:54 +0200

    test app

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { ReactMultiImageComparison } from "./ReactMultiImageComparison";
+import "./style.scss";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+const image1 = new URL("images/1.jpg", import.meta.url);
+const image2 = new URL("images/2.jpg", import.meta.url);
+const image3 = new URL("images/3.jpg", import.meta.url);
+const image4 = new URL("images/3.jpg", import.meta.url);
+
+root.render(
+  <div style={{ width: "1000px" }}>
+    <StrictMode>
+      <ReactMultiImageComparison
+        imagePaths={[image1, image2, image3, image4]}
+        imageDescs={["Basic", "Regular", "Advanced", "Optimized"]}
+      />
+    </StrictMode>
+  </div>
+);
