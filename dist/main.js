6020f804bb83c4d844ac6396c4dd4b8bd88f76b4
Nikola Majksner <nikola@airshaper.com>
2022-06-17 10:36:51 +0200

    build

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,6 @@ var $2Spwt$swchelperslib_to_arrayjs = require("@swc/helpers/lib/_to_array.js");
 var $2Spwt$reactjsxruntime = require("react/jsx-runtime");
 var $2Spwt$react = require("react");
 var $2Spwt$proptypes = require("prop-types");
-var $2Spwt$swchelperslib_define_propertyjs = require("@swc/helpers/lib/_define_property.js");
 
 function $parcel$interopDefault(a) {
   return a && a.__esModule ? a.default : a;
@@ -22,18 +21,11 @@ $parcel$export(module.exports, "ReactMultiImageComparison", function () { return
 
 
 
-
 function $6ed78c020e6666f8$export$e687473b8d6c8292(param) {
-    var totalWidth = param.totalWidth, totalHeight = param.totalHeight, totalImages = param.totalImages, zIndex = param.zIndex, listWidths = param.listWidths, setListWidths = param.setListWidths, updateDesc = param.updateDesc;
-    var initialDimensions = Array.from({
-        length: totalImages - 1
-    }, function(_, i) {
-        return totalWidth / totalImages * (i + 1) - 15;
-    });
+    var totalWidth = param.totalWidth, totalHeight = param.totalHeight, totalImages = param.totalImages, zIndex = param.zIndex, listRefs = param.listRefs, updateDesc = param.updateDesc;
+    var ref1 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(null), 2), currentHandle = ref1[0], setCurrentHandle = ref1[1];
     var handleRefs = (0, $2Spwt$react.useRef)([]);
-    var ref3 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(null), 2), activeHandle = ref3[0], setActiveHandle = ref3[1];
-    var ref1 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(initialDimensions), 2), handlePositions = ref1[0], setHandlePositions = ref1[1];
-    var ref2 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(Array(totalImages - 1).fill(0)), 2), handleOffsets = ref2[0], setHandleOffsets = ref2[1];
+    var handleRef = (0, $2Spwt$react.useRef)([]);
     (0, $2Spwt$react.useEffect)(function() {
         document.addEventListener("mousemove", moveEvent);
         document.addEventListener("mouseup", onEnd);
@@ -47,34 +39,34 @@ function $6ed78c020e6666f8$export$e687473b8d6c8292(param) {
         };
     });
     var onMouseDown = function(e, i) {
-        setActiveHandle(i);
-        setHandleOffsets(Object.assign([], handleOffsets, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, i, e.pageX - handleRefs.current[i].offsetLeft)));
+        setCurrentHandle(i);
+        handleRefs.current[i].offset = e.pageX - handleRefs.current[i].offsetLeft;
     };
     var onEnd = function(e) {
-        setActiveHandle(null);
+        setCurrentHandle(null);
     };
     var touchStart = function(e, i) {
-        setActiveHandle(i);
-        setHandleOffsets(Object.assign([], handleOffsets, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, i, e.pageX - handleRefs.current[i].offsetLeft)));
+        setCurrentHandle(i);
+        handleRefs.current[i].offset = e.pageX - handleRefs.current[i].offsetLeft;
     };
     var moveEvent = function(e) {
-        if (activeHandle !== null) {
-            var handle = handleRefs.current[activeHandle];
-            var handlePosition = Math.min(Math.max(e.touches ? e.touches[0].clientX : e.clientX - handleOffsets[activeHandle], 0 - handle.clientWidth / 2), totalWidth - handle.clientWidth / 2);
+        if (currentHandle !== null) {
+            var handle = handleRefs.current[currentHandle];
+            var handlePosition = Math.min(Math.max(e.touches ? e.touches[0].clientX : e.clientX - handle.offset, 0 - handle.clientWidth / 2), totalWidth - handle.clientWidth / 2);
             var elementPosition = handlePosition + handle.clientWidth / 2;
-            setListWidths(Object.assign([], listWidths, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, activeHandle, elementPosition)));
-            setHandlePositions(Object.assign([], handlePositions, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, activeHandle, handlePosition)));
-            handlePositions.forEach(function(h, index) {
-                if (activeHandle > index && handle.offsetLeft <= h) {
-                    setHandlePositions(Object.assign([], handlePositions, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, index, handlePosition + 2)));
-                    setListWidths(Object.assign([], listWidths, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, index, handlePosition + 15)));
+            handle.style.left = "".concat(handlePosition, "px");
+            listRefs.current[currentHandle].style.width = "".concat(elementPosition, "px");
+            handleRefs.current.forEach(function(h, index) {
+                if (currentHandle > index && handle.offsetLeft <= h.offsetLeft) {
+                    h.style.left = "".concat(handlePosition, "px");
+                    listRefs.current[index].style.width = "".concat(handlePosition + 15, "px");
                 }
-                if (activeHandle < index && handle.offsetLeft >= h) {
-                    setHandlePositions(Object.assign([], handlePositions, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, index, handlePosition + 2)));
-                    setListWidths(Object.assign([], listWidths, (0, ($parcel$interopDefault($2Spwt$swchelperslib_define_propertyjs)))({}, index, handlePosition + 15)));
+                if (currentHandle < index && handle.offsetLeft >= h.offsetLeft) {
+                    h.style.left = "".concat(handlePosition, "px");
+                    listRefs.current[index].style.width = "".concat(handlePosition + 15, "px");
                 }
             });
-            updateDesc(handlePositions);
+            updateDesc(handleRefs);
         }
     };
     var handlePosition1 = function(i) {
@@ -85,14 +77,9 @@ function $6ed78c020e6666f8$export$e687473b8d6c8292(param) {
         handleHeightFirstToLast = (totalImages - 1) * handleDistance;
         return 50 + handleHeightFirstToLast / 2 - i * handleDistance;
     };
-    (0, $2Spwt$react.useEffect)(function() {
-        setHandlePositions(initialDimensions);
-    }, [
-        totalWidth,
-        totalHeight
-    ]);
     return /*#__PURE__*/ (0, $2Spwt$reactjsxruntime.jsx)("div", {
         className: "handles",
+        ref: handleRef,
         children: Array(totalImages - 1).fill(0).map(function(_, i) {
             return /*#__PURE__*/ (0, $2Spwt$reactjsxruntime.jsx)("div", {
                 ref: function(ref) {
@@ -107,7 +94,7 @@ function $6ed78c020e6666f8$export$e687473b8d6c8292(param) {
                 className: "handle",
                 style: {
                     zIndex: totalImages - 2 * i + zIndex,
-                    left: "".concat(handlePositions[i], "px"),
+                    left: "".concat(totalWidth / totalImages * (i + 1) - 15, "px"),
                     top: "".concat(handlePosition1(i), "%")
                 }
             }, i);
@@ -119,38 +106,31 @@ $6ed78c020e6666f8$export$e687473b8d6c8292.propTypes = {
     totalHeight: (0, ($parcel$interopDefault($2Spwt$proptypes))).number.isRequired,
     totalImages: (0, ($parcel$interopDefault($2Spwt$proptypes))).number.isRequired,
     zIndex: (0, ($parcel$interopDefault($2Spwt$proptypes))).number.isRequired,
-    listWidths: (0, ($parcel$interopDefault($2Spwt$proptypes))).array.isRequired,
-    setListWidths: (0, ($parcel$interopDefault($2Spwt$proptypes))).func.isRequired,
     updateDesc: (0, ($parcel$interopDefault($2Spwt$proptypes))).func
 };
 
 
 function $d23fa47777f53b91$export$b4fd821e44278c80(param1) {
     var imagePaths = param1.imagePaths, imageDescs = param1.imageDescs, zIndex = param1.zIndex;
-    var textRefs = (0, $2Spwt$react.useRef)([]);
+    var listRefs = (0, $2Spwt$react.useRef)([]);
     var listRef = (0, $2Spwt$react.useRef)(null);
-    var ref4 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(Array(imagePaths.length).fill(0)), 2), descLeft = ref4[0], setDescLeft = ref4[1];
-    var ref1 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)([]), 2), listWidths = ref1[0], setListWidths = ref1[1];
-    var ref2 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(0), 2), totalWidth = ref2[0], setTotalWidth = ref2[1];
-    var ref3 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(0), 2), totalHeight = ref3[0], setTotalHeight = ref3[1];
-    var initialListWidths = Array.from({
-        length: imagePaths.length
-    }, function(_, i) {
-        return totalWidth / imagePaths.length * (i + 1);
-    });
-    var updateDesc = function(handlePositions) {
-        setDescLeft(descLeft.map(function(_item, idx) {
-            return textPosition(idx, handlePositions);
-        }));
+    var ref3 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(Array(imagePaths.length).fill(0)), 2), descLeft = ref3[0], setDescLeft = ref3[1];
+    var ref1 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(0), 2), totalWidth = ref1[0], setTotalWidth = ref1[1];
+    var ref2 = (0, ($parcel$interopDefault($2Spwt$swchelperslib_sliced_to_arrayjs)))((0, $2Spwt$react.useState)(0), 2), totalHeight = ref2[0], setTotalHeight = ref2[1];
+    var updateDesc = function(handleRefs) {
+        var updated = descLeft.map(function(_item, idx) {
+            return textPosition(idx, handleRefs);
+        });
+        setDescLeft(updated);
     };
-    var textPosition = function(i, handlePositions) {
-        var textWidth = textRefs.current[i].clientWidth;
+    var textPosition = function(i, handleRefs) {
+        var textWidth = listRefs.current[i].lastChild.clientWidth;
         // first
-        if (i === 0) return listWidths[i] / 2 - textWidth / 2;
+        if (i === 0) return listRefs.current[0].clientWidth / 2 - textWidth / 2;
         //last
-        if (imagePaths.length - 1 === i) return (listWidths[i] + handlePositions[i - 1]) / 2 - textWidth / 2;
+        if (imagePaths.length - 1 === i) return (listRefs.current[i].clientWidth + handleRefs.current[i - 1].offsetLeft) / 2 - textWidth / 2;
         // all others except first and last
-        return (handlePositions[i - 1] + handlePositions[i]) / 2 - textWidth / 4;
+        return (handleRefs.current[i - 1].offsetLeft + handleRefs.current[i].offsetLeft) / 2 - textWidth / 4;
     };
     (0, $2Spwt$react.useEffect)(function() {
         var listElement = listRef.current;
@@ -166,14 +146,9 @@ function $d23fa47777f53b91$export$b4fd821e44278c80(param1) {
     }, []);
     (0, $2Spwt$react.useEffect)(function() {
         var initialDescPositions = descLeft.map(function(_item, i) {
-            return totalWidth / imagePaths.length * (2 * i + 1) * 0.5 - textRefs.current[i].clientWidth / 2;
+            return totalWidth / imagePaths.length * (2 * i + 1) * 0.5 - listRefs.current[i].lastChild.clientWidth / 2;
         });
         setDescLeft(initialDescPositions);
-    }, [
-        totalWidth
-    ]);
-    (0, $2Spwt$react.useEffect)(function() {
-        setListWidths(initialListWidths);
     }, [
         totalWidth
     ]);
@@ -187,9 +162,12 @@ function $d23fa47777f53b91$export$b4fd821e44278c80(param1) {
                 ref: listRef,
                 children: imagePaths.map(function(image, i) {
                     return /*#__PURE__*/ (0, $2Spwt$reactjsxruntime.jsxs)("li", {
+                        ref: function(ref) {
+                            return listRefs.current[i] = ref;
+                        },
                         style: {
                             zIndex: imagePaths.length - (2 * i + 1) + zIndex,
-                            width: "".concat(listWidths[i], "px")
+                            width: "".concat(totalWidth / imagePaths.length * (i + 1), "px")
                         },
                         children: [
                             /*#__PURE__*/ (0, $2Spwt$reactjsxruntime.jsx)("img", {
@@ -200,9 +178,6 @@ function $d23fa47777f53b91$export$b4fd821e44278c80(param1) {
                                 }
                             }),
                             imageDescs.length > 0 && /*#__PURE__*/ (0, $2Spwt$reactjsxruntime.jsx)("span", {
-                                ref: function(ref) {
-                                    return textRefs.current[i] = ref;
-                                },
                                 style: {
                                     left: "".concat(descLeft[i], "px")
                                 },
@@ -215,8 +190,7 @@ function $d23fa47777f53b91$export$b4fd821e44278c80(param1) {
             /*#__PURE__*/ (0, $2Spwt$reactjsxruntime.jsx)((0, $6ed78c020e6666f8$export$e687473b8d6c8292), {
                 totalWidth: totalWidth,
                 totalHeight: totalHeight,
-                listWidths: listWidths,
-                setListWidths: setListWidths,
+                listRefs: listRefs,
                 updateDesc: updateDesc,
                 totalImages: imagePaths.length,
                 zIndex: zIndex
